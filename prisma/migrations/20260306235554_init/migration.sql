e44e4e20676bdcb9cbf574ef57cd17b100de5f72
Gurjyot Singh Raina <gurjyotr.cse21@sbjit.edu.in>
2026-03-07 05:47:23 +0530

    "dev: Database setup using Prisma"

---
+++ prisma/migrations/20260306235554_init/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "Event" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "location" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "category" TEXT NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+    "creatorId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Event_pkey" PRIMARY KEY ("id")
+);
