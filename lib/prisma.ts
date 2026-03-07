80f8d00cfd46bfbd5c306f0c4b83af518cb96534
Gurjyot Singh Raina <gurjyotr.cse21@sbjit.edu.in>
2026-03-07 20:19:31 +0530

    fix: Fixed prisma neon adapter config

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,9 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+
+const adapter = new PrismaNeon({
+  connectionString: process.env.DATABASE_URL!,
+});
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
@@ -7,6 +12,7 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
+    adapter,
     log: ["error"],
   });
 
