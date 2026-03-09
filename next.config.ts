5af3460d540fbb004e8e2e73d0cd86a2443611cc
Gurjyot Singh Raina <gurjyotr.cse21@sbjit.edu.in>
2026-03-09 14:45:18 +0530

    fix turbopack uploadthing build

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   turbopack: {
-    rules: {},
+    rules: {
+      "*.md": {
+        loaders: ["ignore-loader"],
+      },
+      "*.cts": {
+        loaders: ["ignore-loader"],
+      },
+    },
   },
 };
 
