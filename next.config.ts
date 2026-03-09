baad338def3c8627f9c567d3bdf90d9ce0d42a39
Gurjyot Singh Raina <gurjyotr.cse21@sbjit.edu.in>
2026-03-09 15:15:22 +0530

    disable turbopack build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
- transpilePackages: ["uploadthing", "@uploadthing/react"],
+  /* config options here */
 };
 
 export default nextConfig;
